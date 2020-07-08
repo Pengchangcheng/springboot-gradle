f004ea0057d2a382d15a51a5f7a3158031db3237
pengchangcheng <changcheng.peng@myt11.com>
2020-07-08 20:13:48 +0800

    配置多环境打包-踩坑，打包可用于发布

--- demo-web/src/test/java/com/pcc/demoweb/DemoWebApplicationTests.java
+++ demo-web/src/test/java/com/pcc/demoweb/DemoWebApplicationTests.java
@@ -1,20 +1,8 @@
 package com.pcc.demoweb;
 
-import com.pcc.demo.service.CityService;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoWebApplicationTests {
-
-    @Test
-    void contextLoads() {
-
-    }
-
-//    @Test
-//    public void testCity() {
-//        CityService service =
-//    }
 
 }
